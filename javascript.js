b8c157f4755a3457c4a31898dc762f38538a7e35
safried <112030532+safried@users.noreply.github.com>
2022-11-08 10:28:44 -0500

    get grid to be 16 by 16

--- javascript.js
+++ javascript.js
@@ -1,16 +1,9 @@
-const constantNum = 16;
-//const container = document.querySelector(".gridContainer");
+const constantNum = 256;
 const cdiv = document.querySelector('.container');
 
-
-//var divContainer = document.createElement("div");
-//divContainer.id = "divContainer";
 for (i = 0; i < constantNum; i++){
     let divChild = document.createElement("div");
     divChild.className = "divChild";
-    //divChild.text = "#";
-    //document.body.appendChild(divChild);
     cdiv.appendChild(divChild);
 }
-//document.body.appendChild(divContainer);
 
