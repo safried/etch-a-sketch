baa027bc1de3669f3d009d94a26a76ac14cdcc68
safried <112030532+safried@users.noreply.github.com>
2022-11-08 11:05:18 -0500

    get grid colors to change as mouse passes over them

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 const constantNum = 256;
-const cdiv = document.querySelector('.container');
+const containerDiv = document.querySelector('.container');
 
 for (i = 0; i < constantNum; i++){
     let divChild = document.createElement("div");
     divChild.className = "divChild";
-    cdiv.appendChild(divChild);
+    divChild.addEventListener("mouseover", ()=>{
+        divChild.style.backgroundColor = "green";
+    });
+    divChild.addEventListener("mouseout", ()=>{
+        divChild.style.backgroundColor = "purple";
+    })
+    containerDiv.appendChild(divChild);
 }
 
